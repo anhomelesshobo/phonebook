f54c464ed0890c0c7c565ac3928de2dfa1673625
Pierluc Dupont <pierlucdupont01@hotmail.ca>
2020-04-26 14:38:26 -0400

    phonebookDAO added

---
+++ phonebook/ContactModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace phonebook
+{
+    public class ContactModel
+    {
+        public int ContactID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public string Mobile { get; set; }
+    }
+}
